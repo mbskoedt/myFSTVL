f98b3d3aa8a978628fb1815efced9c553ea0cfbe
cplovst <plovst@live.dk>
2019-02-20 11:10:00 +0100

    Revert "Merge branch 'master' of https://github.com/mbskoedt/myFSTVL"
    This reverts commit d09e97c45596b9b7cba8af96bbc9764931523b02, reversing
    changes made to d557dc8a7e5d457b7343e42b45796554038458bd.

--- js/main.js
+++ js/main.js
@@ -85,19 +85,14 @@ setDefaultPage();
 ////////// TINDER CARDS ///////////
 
 
-<<<<<<< HEAD
 ///// klik for like
 var vidDislike = document.getElementById("dislike");
-=======
-///// KLIK FOR LIKE //////
-let vidDislike = document.getElementById("dislike");
->>>>>>> 555e91604323262ec1c860c6254f75d19e84f12e
 
 function playVidDislike() {
   vidDislike.play();
 }
 
-let vidLike = document.getElementById("like");
+var vidLike = document.getElementById("like");
 
 function playVidLike() {
   vidLike.play();
